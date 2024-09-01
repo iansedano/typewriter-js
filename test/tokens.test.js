0ce13a91ae3fc0c0bab7c240b43d8651ec2c8625
Ian Currie <iansedano@gmail.com>
2024-09-01 15:04:36 +0200

    Add test to tokenize nested element with punctuation

--- test/tokens.test.js
+++ test/tokens.test.js
@@ -1,7 +1,7 @@
 import { tokenizeHTML } from "../src/tokens";
 import { expect, test } from "vitest";
 
-test("tokenizeHTML", () => {
+test("Tokenize basic HTML string with nested span", () => {
   const html = '<div class="foo">Hello <span>world</span></div>';
   const dom = new DOMParser().parseFromString(html, "text/html");
 
@@ -18,6 +18,39 @@ test("tokenizeHTML", () => {
         className: "",
         children: [
           { type: "sentence", children: [{ type: "word", value: "world" }] },
+        ],
+      },
+    ],
+  });
+});
+
+test("Tokenize P element with nested EM element, followed immediately by comma", () => {
+  const html = '<p class="foo">lingua, <em>institutis</em>, legibus</p>';
+  const dom = new DOMParser().parseFromString(html, "text/html");
+
+  const tokens = tokenizeHTML(dom.body.firstChild);
+  expect(tokens).toEqual({
+    type: "element",
+    tagName: "P",
+    className: "foo",
+    children: [
+      { type: "sentence", children: [{ type: "word", value: "lingua, " }] },
+      {
+        type: "element",
+        tagName: "EM",
+        className: "",
+        children: [
+          {
+            type: "sentence",
+            children: [{ type: "word", value: "institutis" }],
+          },
+        ],
+      },
+      {
+        type: "sentence",
+        children: [
+          { type: "word", value: ", " },
+          { type: "word", value: "legibus" },
         ],
       },
     ],
